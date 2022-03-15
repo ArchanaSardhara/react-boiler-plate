1714ea42708fd6fe009f2239daf2af904eea655d
Archana Sardhara <archnasardhara@gmail.com>
2022-03-15 14:51:24 +0530

    fix manifest

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -11,7 +12,7 @@ module.exports = {
     open: true,
     compress: true,
     hot: true,
-    port: 8080,
+    port: 3004,
   },
   entry: {
     main: path.resolve(__dirname, "./src/index.js"),
@@ -19,6 +20,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "[name].bundle.js",
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -31,9 +33,25 @@ module.exports = {
         test: /\.(scss|css)$/,
         use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
       },
+      {
+        test: /\.(json|svg|png|jpg|jpeg|woff|woff2|ttf|eot|ico)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          limit: 10000,
+        },
+      },
     ],
   },
   plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./public"),
+          to: path.resolve(__dirname, "./dist"),
+        },
+      ],
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
