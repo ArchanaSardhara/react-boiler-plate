3bec3a003005d55a306231bdb15e9df114a1d0a9
Archana Sardhara <archnasardhara@gmail.com>
2022-03-17 14:57:23 +0530

    theme

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import {
-  Row,
-  Col,
   Collapse,
   Navbar,
   NavbarToggler,
@@ -17,11 +15,16 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   const [isOpen, toggleCollapse] = useState(false);
+
   const toggle = () => {
     toggleCollapse(!isOpen);
   };
+  const handleChangeTheme = (theme) => {
+    document.body.setAttribute("data-theme", theme);
+  };
+
   return (
-    <Navbar className="header-container" color="dark" dark expand="md">
+    <Navbar className="header-container" expand="md">
       <NavbarBrand href="/">
         <span className="logo">
           <img src="assets/img/logo.png" alt="logo" /> Todo App
@@ -31,12 +34,12 @@ const Header = () => {
       <Collapse isOpen={isOpen} navbar>
         <Nav className="ms-auto" navbar>
           <NavItem>
-            <Link className="nav-link text-white" to="/">
+            <Link className="nav-link" to="/">
               Home
             </Link>
           </NavItem>
           <NavItem>
-            <Link className="nav-link text-white" to="/todo">
+            <Link className="nav-link" to="/todo">
               Todo
             </Link>
           </NavItem>
@@ -45,9 +48,14 @@ const Header = () => {
               Settings
             </DropdownToggle>
             <DropdownMenu>
-              <DropdownItem>Profile</DropdownItem>
+              <DropdownItem>Theme</DropdownItem>
               <DropdownItem divider />
-              <DropdownItem>Logout</DropdownItem>
+              <DropdownItem onClick={() => handleChangeTheme("dark")}>
+                Dark
+              </DropdownItem>
+              <DropdownItem onClick={() => handleChangeTheme("light")}>
+                Light
+              </DropdownItem>
             </DropdownMenu>
           </UncontrolledDropdown>
         </Nav>
